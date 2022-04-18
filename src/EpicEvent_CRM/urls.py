0680f973a972effab172b722f2ba4ad15c7982c9
Guillaume Postec <gg.postec@laposte.net>
2022-04-18 21:35:42 +0200

    Création des urls avec nested routers pour api

--- src/EpicEvent_CRM/urls.py
+++ src/EpicEvent_CRM/urls.py
@@ -14,8 +14,41 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.routers import SimpleRouter
+from rest_framework_nested.routers import NestedSimpleRouter
+
+from api.views import EventViewSet, ContractViewSet, ClientViewSet, EmployeeViewSet, CompanyViewSet, GroupViewSet
+
+
+router = SimpleRouter()
+router.register(r'events', EventViewSet, basename='events')
+
+router.register(r'contracts', ContractViewSet, basename='contracts')
+
+router.register(r'employees', EmployeeViewSet, basename='employees')
+
+router.register(r'clients', ClientViewSet, basename='clients')
+
+router.register(r'companies', CompanyViewSet, basename='companies')
+company_router = NestedSimpleRouter(router, r'companies', lookup='company')
+company_router.register(r'clients', ClientViewSet, basename='company-clients')
+
+router.register(r'groups', GroupViewSet, basename='groups')
+groups_router = NestedSimpleRouter(router, r'groups', lookup='group')
+groups_router.register(r'employees', EmployeeViewSet, basename='group-employees')
+group_employees_router = NestedSimpleRouter(groups_router, r'employees', lookup='employee')
+group_employees_router.register(r'contracts', ContractViewSet, basename='saler-contracts')
+group_employees_router.register(r'clients', ClientViewSet, basename='saler-clients')
+group_employees_router.register(r'events', EventViewSet, basename='support-events')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include(router.urls)),
+    path('core/', include('core.urls'))
 ]
