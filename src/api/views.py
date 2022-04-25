0e34916dec03d9563a27936b7ceaa97af6526dc2
Guillaume Postec <gg.postec@laposte.net>
2022-04-25 18:47:23 +0200

    Views implementation

--- src/api/views.py
+++ src/api/views.py
@@ -1,56 +1,141 @@
 from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
-from core.models import Address, Company, Employee, Client, Event, Contract, Group
+from core.models import Address, Company, Employee, Client, Event, Contract
+from core.permissions import CanGet, CanCreate, CanUpdate, CanDestroy
 from .serializers import *
 
 # Create your views here.
-# class ProjectViewSet(ModelViewSet):
-#     serializer_class = ProjectDetailSerializer
-#     list_serializer_class = ProjectListSerializer
-
-#     def get_permissions(self):
-#         if self.action == ('update' or 'destroy'):
-#             self.permission_classes = [IsAuthorOrOwner, ]
-#         else:
-#             self.permission_classes = [IsContributor, ]
-#         return super().get_permissions()
-
-#     def get_queryset(self):
-#         user = self.request.user
-#         queryset = Project.objects.all()
-#         if user.is_superuser:
-#             return queryset
-#         else:
-#             user_contributions = Contributor.objects.filter(user=user.id)
-#             user_contributions_id_list = []
-#             for contribution in user_contributions:
-#                 user_contributions_id_list.append(contribution.project.id)
-#             print(user_contributions_id_list)
-#             queryset = queryset.filter(id__in=user_contributions_id_list)
-#             return queryset
-
-#     def get_serializer_class(self):
-#         if self.action == 'list':
-#             return self.list_serializer_class
-#         return super().get_serializer_class()
-
-
-class EventViewSet(ModelViewSet):
-    pass
-
-
-class ContractViewSet(ModelViewSet):
-    pass
+def check_permissions(method, action):
+    if method == 'GET':
+        return [CanGet]
+    elif action == 'create':
+        return [CanCreate]
+    elif action == 'update':
+        return [CanUpdate]
+    elif action == 'destroy':
+        return [CanDestroy]
 
 
 class EmployeeViewSet(ModelViewSet):
-    pass
+    serializer_class = EmployeeDetailSerializer
+    list_serializer_class = EmployeeListSerializer
+
+    def get_permissions(self):
+        method = self.request.method
+        action = self.action
+        self.permission_classes = check_permissions(method, action)
+        return super().get_permissions()
+
+    def get_queryset(self):
+        queryset = Employee.objects.all()
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return self.list_serializer_class
+        return super().get_serializer_class()
+
+
+class ManagementEmployeesViewSet(EmployeeViewSet):
+        def get_queryset(self):
+            management_goup_id = 4
+            if self.action == 'list':
+                queryset = Client.objects.filter(group__contains=management_goup_id)
+            else:
+                raise Exception('Only list action request can be use on this request')
+            return queryset
 
 
 class ClientViewSet(ModelViewSet):
-    pass
+    serializer_class = ClientDetailSerializer
+    list_serializer_class = ClientListSerializer
+
+    def get_permissions(self):
+        method = self.request.method
+        action = self.action
+        self.permission_classes = check_permissions(method, action)
+        return super().get_permissions()
+
+    def get_queryset(self):
+        queryset = Client.objects.all()
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return self.list_serializer_class
+        return super().get_serializer_class()
+
+
+class MyClientsViewSet(ClientViewSet):
+        def get_queryset(self):
+            if self.action == 'list':
+                queryset = Client.objects.filter(sales_contact=self.request.user.id)
+            else:
+                raise Exception('Only list action request can be use on this request')
+            return queryset
+
+
+class ContractViewSet(ModelViewSet):
+    serializer_class = ContractDetailSerializer
+    list_serializer_class = ContractListSerializer
+
+    def get_permissions(self):
+        method = self.request.method
+        action = self.action
+        self.permission_classes = check_permissions(method, action)
+        return super().get_permissions()
+
+    def get_queryset(self):
+        queryset = Contract.objects.all()
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return self.list_serializer_class
+        return super().get_serializer_class()
+
+
+class MyContractsViewSet(ContractViewSet):
+        def get_queryset(self):
+            if self.action == 'list':
+                my_contracts_queryset = Contract.objects.none()
+                all_contracts_queryset = Contract.objects.all()
+                for contract in all_contracts_queryset:
+                    if contract.client == self.request.user.id:
+                        my_contracts_queryset.append(contract)
+            else:
+                raise Exception('Only list action request can be use on this request')
+            return my_contracts_queryset
+
+
+class EventViewSet(ModelViewSet):
+    serializer_class = EventDetailSerializer
+    list_serializer_class = EventListSerializer
+
+    def get_permissions(self):
+        method = self.request.method
+        action = self.action
+        self.permission_classes = check_permissions(method, action)
+        return super().get_permissions()
+
+    def get_queryset(self):
+        queryset = Event.objects.all()
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return self.list_serializer_class
+        return super().get_serializer_class()
+
+
+class MyEventsViewSet(ClientViewSet):
+        def get_queryset(self):
+            if self.action == 'list':
+                queryset = Client.objects.filter(support_contact=self.request.user.id)
+            else:
+                raise Exception('Only list action request can be use on this request')
+            return queryset
 
 
 class CompanyViewSet(ModelViewSet):
@@ -62,16 +147,4 @@ class GroupViewSet(ModelViewSet):
 
 
 class AddressViewSet(ModelViewSet):
-    serializer_class = AddressDetailSerializer
-    list_serializer_class = AdressListSerializer
-
-    def get_permissions(self):
-        if self.action == ('update' or 'destroy'):
-            self.permission_classes = []
-        else:
-            self.permission_classes = []
-        return super().get_permissions()
-
-    def get_queryset(self):
-        user = self.request.user
-        queryset = Address.objects.all()
+    pass
